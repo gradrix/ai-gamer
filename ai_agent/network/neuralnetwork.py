b6237536f108fc65c397bee36308e8372331b894
Gražvydas J <grazjuc@gmail.com>
2024-12-14 18:10:12 +0200

    some changes

---
+++ ai_agent/network/neuralnetwork.py
@@ -0,0 +1,103 @@
+import os
+import logging
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras import layers
+from tensorflow.keras.models import load_model
+import random
+
+logger = logging.getLogger(__name__)
+
+# Hyperparameters
+gamma = 0.95  # Discount factor
+alpha = 0.001  # Learning rate
+epsilon = 1.0  # Exploration-exploitation trade-off
+epsilon_min = 0.01
+epsilon_decay = 0.995
+batch_size = 64
+num_actions = 1000  # Number of possible moves
+input_shape = (100, 100, 1)
+
+class NeuralNetwork:
+
+    def __init__(self, agentName):
+        self.model_file = 'data/models/' +agentName + '.keras'
+        self.loadModel()
+
+    def loadModel(self):
+        if os.path.exists(self.model_file):
+            logger.info("Loading existing NN model from the disk.")
+            self.model = load_model(self.model_file)
+        else:
+            self.model = self.build_model()
+            self.save()
+
+    def save(self):
+        self.model.save(self.model_file)
+
+    def build_model(self):
+        model = tf.keras.Sequential()
+        model.add(tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=input_shape))
+        model.add(tf.keras.layers.MaxPooling2D((2, 2)))
+        model.add(tf.keras.layers.Conv2D(64, (3, 3), activation='relu'))
+        model.add(tf.keras.layers.MaxPooling2D((2, 2)))
+        model.add(tf.keras.layers.Flatten())
+        model.add(tf.keras.layers.Dense(128, activation='relu'))
+        model.add(tf.keras.layers.Dense(num_actions, activation='linear'))  # Outputs Q-values
+        model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=alpha), loss='mse')
+        return model
+    
+    def get_model(self):
+        return self.model
+
+class ReplayBuffer:
+    def __init__(self, max_size):
+        self.buffer = []
+        self.max_size = max_size
+    
+    def add(self, experience):
+        if len(self.buffer) >= self.max_size:
+            self.buffer.pop(0)
+        self.buffer.append(experience)
+    
+    def sample(self, batch_size):
+        return random.sample(self.buffer, batch_size)
+
+
+class DQNAgent:
+    def __init__(self, nn: NeuralNetwork):
+        self.model = nn.get_model()
+        self.target_model = nn.get_model()
+        self.replay_buffer = ReplayBuffer(max_size=2000)
+        self.epsilon = epsilon
+        
+    def act(self, state):
+        if np.random.rand() <= self.epsilon:
+            return np.random.randint(0, num_actions)  # Random move (exploration)
+        q_values = self.model.predict(state)
+        return np.argmax(q_values[0])  # Best move (exploitation)
+    
+    def remember(self, state, action, reward, next_state, done):
+        self.replay_buffer.add((state, action, reward, next_state, done))
+
+    def replay(self):
+        if len(self.replay_buffer.buffer) < batch_size:
+            return
+        
+        minibatch = self.replay_buffer.sample(batch_size)
+        for state, action, reward, next_state, done in minibatch:
+            target = reward
+            if not done:
+                target = reward + gamma * np.amax(self.target_model.predict(next_state)[0])
+            
+            q_values = self.model.predict(state)
+            q_values[0][action] = target  # Update the Q-value for the chosen action
+            
+            self.model.fit(state, q_values, epochs=1, verbose=0)
+
+        # Decay epsilon (less random exploration over time)
+        if self.epsilon > epsilon_min:
+            self.epsilon *= epsilon_decay
+
+    def update_target_network(self):
+        self.target_model.set_weights(self.model.get_weights())
--- run_tests.sh
+++ run_tests.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-python3 -m pytest tests/**.py
+# Stop and remove the existing game_tests container (if it exists)
+docker stop game_tests >/dev/null 2>&1 && docker rm game_tests >/dev/null 2>&1
+
+# Build the new ai_agent image
+docker build -t game_tests -f ./ai_agent/Dockerfile.test .
+
+# Run the new ai_agent container
+docker run -it --network host --gpus all -d -v $(pwd)/data/models:/app/data/models --name game_tests game_tests
